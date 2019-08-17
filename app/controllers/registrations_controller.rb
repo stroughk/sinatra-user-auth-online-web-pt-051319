4c56c8a369384fe17aa5006ddc5719921e132d5b
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-17 22:40:19 +0000

    Automatically backed up by Learn

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < ApplicationController
     erb :'/registrations/signup'
   end
 
-  post '/registrations' do 
+  post '/registrations' do
     # use the data in params to create a new user and log them in by
     # setting the session[:id] equal to the user's id here
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
@@ -13,7 +13,7 @@ class RegistrationsController < ApplicationController
     session[:id] = @user.id
 
     # this redirect takes us to the route: get '/users/home' that is in the Users Controller
-    #   go and look at that route in the Users Controller. 
+    #   go and look at that route in the Users Controller.
     redirect '/users/home'
   end
 
